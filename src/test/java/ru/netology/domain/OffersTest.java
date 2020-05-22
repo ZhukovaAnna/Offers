78298c21f17e87da202aa92432127a704c380881
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-05-22 16:56:39 +0300

    Add repository test

--- src/test/java/ru/netology/domain/OffersTest.java
+++ src/test/java/ru/netology/domain/OffersTest.java
@@ -7,14 +7,14 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OffersTest {
-Offers offers1=new Offers(1,1000,"VKO","LED",85);
-Offers offers2=new Offers(2,1500,"VKO","LED",90);
-Offers offers3=new Offers(3,2000,"DME","LED",90);
+    Offers offers1 = new Offers(1, 1000, "VKO", "LED", 85);
+    Offers offers2 = new Offers(2, 1500, "VKO", "LED", 90);
+    Offers offers3 = new Offers(3, 2000, "DME", "LED", 90);
 
     @Test
     void shouldSortByPrice() {
-        Offers[] expected = new Offers[] {offers1, offers2, offers3};
-        Offers[] actual = new Offers[] {offers3, offers2, offers1};
+        Offers[] expected = new Offers[]{offers1, offers2, offers3};
+        Offers[] actual = new Offers[]{offers3, offers2, offers1};
 
         Arrays.sort(actual);
 
--- src/test/java/ru/netology/manager/OffersManagerTest.java
+++ src/test/java/ru/netology/manager/OffersManagerTest.java
@@ -7,24 +7,24 @@ import ru.netology.repository.OffersRepository;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OffersManagerTest {
-    OffersRepository repository= new OffersRepository();
-    OffersManager manager=new OffersManager(repository);
+    OffersRepository repository = new OffersRepository();
+    OffersManager manager = new OffersManager(repository);
 
-    Offers offers1=new Offers(1,1000,"VKO","LED",80);
-    Offers offers2=new Offers(2,1500,"LED","VKO",80);
-    Offers offers3=new Offers(3,3000,"DME","KZN",90);
-    Offers offers4 =new Offers(4,3500,"VKO","LED",95);
+    Offers offers1 = new Offers(1, 1000, "VKO", "LED", 80);
+    Offers offers2 = new Offers(2, 1500, "LED", "VKO", 80);
+    Offers offers3 = new Offers(3, 3000, "DME", "KZN", 90);
+    Offers offers4 = new Offers(4, 3500, "VKO", "LED", 95);
 
     @Test
-    void shouldReturnEmptyIfNoTicket(){
-        manager=new OffersManager(repository);
-        Offers[]expected = new Offers[0];
-        Offers[]actual = manager.findAll("VKO","LED");
-        assertArrayEquals(expected,actual);
+    void shouldReturnEmptyIfNoTicket() {
+        manager = new OffersManager(repository);
+        Offers[] expected = new Offers[0];
+        Offers[] actual = manager.findAll("VKO", "LED");
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    void shouldReturnTicketIfFind(){
+    void shouldReturnTicketIfFind() {
         manager = new OffersManager(repository);
         manager.offersAdd(offers1);
         manager.offersAdd(offers2);
@@ -34,15 +34,25 @@ class OffersManagerTest {
     }
 
     @Test
-    void shouldSortTicketIfFind(){
-        manager=new OffersManager(repository);
+    void shouldSortTicketIfFind() {
+        manager = new OffersManager(repository);
         manager.offersAdd(offers1);
         manager.offersAdd(offers2);
         manager.offersAdd(offers3);
         manager.offersAdd(offers4);
-        Offers[]expected = new Offers[]{offers1,offers4};
-        Offers[] actual = manager.findAll("VKO","LED");
-        assertArrayEquals(expected,actual);
+        Offers[] expected = new Offers[]{offers1, offers4};
+        Offers[] actual = manager.findAll("VKO", "LED");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnEmptyIfNotContain() {
+        manager = new OffersManager(repository);
+        manager.offersAdd(offers1);
+        manager.offersAdd(offers2);
+        Offers[] expected = new Offers[0];
+        Offers[] actual = manager.findAll("DME", "KZN");
+        assertArrayEquals(expected, actual);
     }
 
 }
